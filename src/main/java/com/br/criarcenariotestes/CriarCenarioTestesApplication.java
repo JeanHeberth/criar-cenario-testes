e329a5371bb48fae2dfcbe7ef5dc5b69bb2aee68
jeanheberth <jeanheberth19@gmail.com>
2025-04-21 17:44:27 -0300

    init

---
+++ src/main/java/com/br/criarcenariotestes/CriarCenarioTestesApplication.java
@@ -0,0 +1,13 @@
+package com.br.criarcenariotestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriarCenarioTestesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriarCenarioTestesApplication.class, args);
+    }
+
+}
