1dec114fe331b684de57840040cbcdbd8a24de9f
jeanheberth <jeanheberth19@gmail.com>
2025-06-30 16:39:44 -0300

    Alterando o serviço de gerar cenários

--- src/main/java/com/br/criarcenariotestes/controller/CenarioController.java
+++ src/main/java/com/br/criarcenariotestes/controller/CenarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://192.168.1.10:4200",
+@CrossOrigin(origins = "http://192.168.1.9:4200",
         originPatterns = "http://localhost:4200")
 @RestController
 @RequestMapping("/cenario")
