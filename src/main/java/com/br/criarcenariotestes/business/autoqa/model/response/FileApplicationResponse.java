74e243edfafc5d9bb19e1575a8d2decc68f125d1
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2026-08-04 12:36:04 -0300

    limpando a branch para um inicio humano

--- src/main/java/com/br/criarcenariotestes/business/autoqa/model/response/FileApplicationResponse.java
+++
@@ -1,9 +0,0 @@
-package com.br.criarcenariotestes.business.autoqa.model.response;
-
-public record FileApplicationResponse(
-        boolean success,
-        String backupId,
-        int appliedCount,
-        String message
-) {
-}
